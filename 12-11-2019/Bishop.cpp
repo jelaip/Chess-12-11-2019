bc570f8f658d123d06c4c874faaf8d5b5e214fae
jelaip <37291886+jelaip@users.noreply.github.com>
2019-11-13 10:41:24 +0100

    rock Bishop Queen

--- 12-11-2019/Bishop.cpp
+++ 12-11-2019/Bishop.cpp
@@ -12,3 +12,49 @@ Bishop::Bishop(std::string nAsset, std::string chemin, int _x, int _y, bool _bla
 Bishop::~Bishop()
 {
 }
+
+std::list<sf::Vector2i>  Bishop::caseMove(std::array< std::array<Pion*, 8>, 8> caseVirtuel) {
+	std::list<sf::Vector2i> m;
+	int i = 0;
+	while (x + i < 8 && y + i < 8) {
+		i++;
+		if (caseVirtuel[x+i][y+i] == nullptr)
+			m.push_back(sf::Vector2i(x+i, y+i));
+		if (caseVirtuel[x+i][y+i] != nullptr && black != caseVirtuel[x+i][y+i]->black) {
+			m.push_back(sf::Vector2i(x+i, y+i));
+			break;
+		}
+	}
+	i = 0;
+	while (x - i > -1 && y + i < 8) {
+		i++;
+		if (caseVirtuel[x - i][y + i] == nullptr)
+			m.push_back(sf::Vector2i(x - i, y + i));
+		if (caseVirtuel[x - i][y + i] != nullptr && black != caseVirtuel[x - i][y + i]->black) {
+			m.push_back(sf::Vector2i(x - i, y + i));
+			break;
+		}
+	}
+	i = 0;
+	while (x - i > -1 && y - i>-1 ) {
+		i++;
+		if (caseVirtuel[x - i][y - i] == nullptr)
+			m.push_back(sf::Vector2i(x - i, y - i));
+		if (caseVirtuel[x - i][y - i] != nullptr && black != caseVirtuel[x - i][y - i]->black) {
+			m.push_back(sf::Vector2i(x - i, y - i));
+			break;
+		}
+	}
+	i = 0;
+	while (x + i < 8 && y - i > -1) {
+		i++;
+		if (caseVirtuel[x + i][y - i] == nullptr)
+			m.push_back(sf::Vector2i(x + i, y - i));
+		if (caseVirtuel[x + i][y - i] != nullptr && black != caseVirtuel[x + i][y - i]->black) {
+			m.push_back(sf::Vector2i(x + i, y - i));
+			break;
+		}
+	}
+	
+	return m;
+}
--- 12-11-2019/Rook.cpp
+++ 12-11-2019/Rook.cpp
@@ -14,9 +14,37 @@ Rook::~Rook()
 }
 std::list<sf::Vector2i>  Rook::caseMove(std::array< std::array<Pion*, 8>, 8> caseVirtuel) {
 	std::list<sf::Vector2i> m;
-	for (int i = x; i < 7; i++) {
-		if ( caseVirtuel[i][y] == nullptr)
+	for (int i = x; i <= 7; i++) {
+		if (caseVirtuel[i][y] == nullptr)
 			m.push_back(sf::Vector2i(i, y));
-
+		if (caseVirtuel[i][y] != nullptr && black != caseVirtuel[i][y]->black) {
+			m.push_back(sf::Vector2i(i, y));
+			break;
+		}
+	}
+	for (int i = x; i >= 0; i--) {
+		if (caseVirtuel[i][y] == nullptr)
+			m.push_back(sf::Vector2i(i, y));
+		if (caseVirtuel[i][y] != nullptr && black != caseVirtuel[i][y]->black) {
+			m.push_back(sf::Vector2i(i, y));
+			break;
+		}
+	}
+	for (int i = y; i <= 7; i++) {
+		if (caseVirtuel[x][i] == nullptr)
+			m.push_back(sf::Vector2i(x, i));
+		if (caseVirtuel[x][i] != nullptr && black != caseVirtuel[x][i]->black) {
+			m.push_back(sf::Vector2i(x, i));
+			break;
+		}
+	}
+	for (int i = y; i >= 0; i--) {
+		if (caseVirtuel[x][i] == nullptr)
+			m.push_back(sf::Vector2i(x, i));
+		if (caseVirtuel[x][i] != nullptr && black != caseVirtuel[x][i]->black) {
+			m.push_back(sf::Vector2i(x, i));
+			break;
+		}
 	}
 	return m;
+}
